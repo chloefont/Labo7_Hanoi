e0d1d73320e583779d0eb38ffe5aa69de06104b5
Luca Coduri <coduri.vitor@gmail.com>
2021-11-25 15:31:14 +0100

    remove extends

--- src/hanoi/HanoiDisplayer.java
+++ src/hanoi/HanoiDisplayer.java
@@ -1,10 +1,7 @@
 package hanoi;
 
-import hanoi.gui.JHanoiDisplay;
+public class HanoiDisplayer {
 
-public class HanoiDisplayer extends JHanoiDisplay {
-
-    @Override
     public void display(Hanoi h){
         System.out.println(h.display());
     }
